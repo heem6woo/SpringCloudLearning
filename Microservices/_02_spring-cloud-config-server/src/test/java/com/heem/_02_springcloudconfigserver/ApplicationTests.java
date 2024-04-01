e8b172a4dc1b3c14c5b53b411efea16a8fd72a37
Heemin Woo <heem6woo@gmail.com>
2024-04-01 15:50:14 +0900

    Feat: Spring Clound Config

---
+++ Microservices/_02_spring-cloud-config-server/src/test/java/com/heem/_02_springcloudconfigserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heem._02_springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
